04df968c0ddbf678eba3cd6a91bd2006d864fbc1
unrealtobi <tobiwunmie@gmail.com>
2025-06-21 13:36:14 +0100

    fix files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Use '/my-thrift-admin/' if deployed to a subdirectory
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 });
